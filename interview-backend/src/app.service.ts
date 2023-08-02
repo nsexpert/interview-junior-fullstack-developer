dfd1a75a1130be2093010cc1e2c15483193c810b
tony <tonyrop00@gmail.com>
2023-08-02 09:09:19 +0300

    completed test

--- interview-backend/src/app.service.ts
+++ interview-backend/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+const cities = require('../../cities.json');
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  postSearch(data: any): any {
+    var keyword = data.keyword;
+    var page = data.page;
+    let resultAll = cities.filter((city: any) => {
+      return city.cityName.includes(keyword);
+    })
+
+    //pagination
+    let resultForPage = resultAll.slice((page - 1) * 5, page * 5);
+
+    return {
+      cities: resultForPage,
+      maxPage: Math.ceil(resultAll.length / 5)
+    };
   }
 }
--- interview-frontend/src/app/app.component.ts
+++ interview-frontend/src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const url = 'http://localhost:3000';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'interview-frontend';
+  title: string;
+  currentPage: number;
+  maxPage: number;
+  keyword: string;
+  cities: any;
+
+  constructor(private http: HttpClient) {
+    this.title = 'interview-frontend';
+    this.currentPage = 0;
+    this.maxPage = 0;
+    this.keyword = "";
+    this.cities = [];
+  }
+
+  handleSearch = () => {
+    if (this.keyword !== "") {
+      this.sendRequest(this.keyword, 1);
+    } else {
+      //init
+      if (this.cities.length > 0) {
+        this.cities = [];
+        this.currentPage = 0;
+        this.maxPage = 0;
+      }
+    }
+  }
+
+  handlePrevPage = () => {
+    this.currentPage--;
+    this.sendRequest(this.keyword, this.currentPage);
+  }
+
+  handleNextPage = () => {
+    this.currentPage++;
+    this.sendRequest(this.keyword, this.currentPage);
+  }
+
+  sendRequest = (keyword: string, page: number) => {
+    this.http.post(url, { keyword: keyword, page: page }).subscribe(
+      (data: any) => {
+        this.cities = data.cities;
+        this.maxPage = data.maxPage;
+        if (this.maxPage == 0) {
+          this.currentPage = 0;
+        } else {
+          if (this.currentPage == 0) {
+            this.currentPage = 1;
+          }
+        }
+      },
+      (error) => {
+        console.error('Request failed:', error);
+      }
+    );
+  }
 }
